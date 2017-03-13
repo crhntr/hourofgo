b28400ba616672de9414de380b489dff36dadad5
crhntr <hunter.christopher@icloud.com>
2017-03-13 11:27:27 -0700

    Added documentation to helloworld.go

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -1,3 +1,17 @@
+// helloworld is a package showing one way to write a hello world program in go
+//    package main
+//
+//    import "fmt"
+//
+//    func main() {
+//    	fmt.Println("Hello world!")
+//    }
+// an alternate way to print helllo world to stdout without importing fmt is
+//    package main
+//
+//    func main() {
+//      print("Hello world!\n")
+//    }
 package main
 
 import "fmt"
